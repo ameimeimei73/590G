638833f54199664d9cc06a15032906c98e4f02ef
Mei <chenmeili070308@gmail.com>
2020-04-24 10:59:45 -0400

    Updata UI start menu

---
+++ Final/Assets/Script/Main_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+	public void scene_loader(int SenceIndex) {
+		SceneManager.LoadScene(SenceIndex);
+	}
+
+    public void quit_game()
+    {
+        Debug.Log ("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ Final/Assets/Script/Settings_Menu.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings_Menu : MonoBehaviour
+{
+    public void set_volume(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
